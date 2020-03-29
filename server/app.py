90990ff806d4fa285a0b4def7e8a72724a58881a
howardwang15 <joy_luck_2000@hotmail.com>
2020-03-29 11:44:40 -0700

    added html templates for flask

--- server/app.py
+++ server/app.py
@@ -1,9 +1,28 @@
-from flask import Flask
+from flask import Flask, request, send_file, render_template
 import os
+import glob
+import mysql.connector
 
 app = Flask(__name__)
 
 
+@app.route('/')
+def test():
+    return render_template('gallery.html')
+
 @app.route('/api/videos')
 def fetch_videos():
-    print('hello')
+    cnx = mysql.connector.connect(user='root', password='howardwang2000', host='mysql', database='surveillance')
+    cursor = cnx.cursor()
+    timestamp = request.args.get('time')
+    # fetch_all = "SELECT * FROM videos"
+    fetch_videos = "SELECT * FROM videos WHERE start_time < '{}'".format(timestamp)
+    cursor.execute(fetch_videos)
+    rows = cursor.fetchall()
+    images = glob.glob('../files/*.png')
+    return render_template('gallery.html', image_names=images)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
+
