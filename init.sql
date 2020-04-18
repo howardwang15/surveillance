b6251c98728c01441259acd741ff1c991eb8c29c
howardwang15 <joy_luck_2000@hotmail.com>
2020-04-18 11:32:41 -0700

    added sql file for initializing database and tables

---
+++ init.sql
@@ -0,0 +1,23 @@
+-- Use these queries to setup new surveillance database
+
+CREATE DATABASE surveillance;
+
+USE surveillance;
+
+CREATE TABLE videos (
+    id int NOT NULL AUTO_INCREMENT,
+    start_time datetime NOT NULL,
+    video_name text NOT NULL,
+    first_frame text NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE detections (
+    id int NOT NULL AUTO_INCREMENT,
+    video_id int NOT NULL,
+    type text NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (video_id) REFERENCES videos(id)
+);
+
+SHOW TABLES;  -- verify that tables were created
