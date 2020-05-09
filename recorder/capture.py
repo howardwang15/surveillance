beba181193223e2685603416f05e6035bd81f038
howardwang15 <joy_luck_2000@hotmail.com>
2020-05-09 12:07:40 -0700

    updated detection to operate on 50% captured frame width instead of 25%

--- recorder/capture.py
+++ recorder/capture.py
@@ -18,15 +18,27 @@ from multiprocessing import Process
 
 
 def get_options():
+    """Add and parse command line arguments
+
+    Returns:
+        argparse.Namespace: options parsed while running program
+    """
     parser = argparse.ArgumentParser()
     parser.add_argument('-w', '--weights', help='path to YOLO weights')
     options = parser.parse_args()
     return options
 
+
 def transform_images(x_train, size=416):
+    """Perform processing on images
+
+    Args:
+        x_train: 
+    """
     x_train = tf.image.resize(x_train, (size, size))
     x_train = x_train / 255
     return x_train
+
 
 def draw_outputs(img, outputs, class_names):
     boxes, objectness, classes, nums = outputs
@@ -39,6 +51,7 @@ def draw_outputs(img, outputs, class_names):
             class_names[int(classes[i])], objectness[i]),
             x1y1, cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0, 0, 255), 2)
     return img
+
 
 class Recorder():
     def __init__(self, weights, tiny, buffer_length=150):
@@ -57,6 +70,7 @@ class Recorder():
             max_size=1024*1024*1,
             num_files=5
         )
+
 
     def capture(self):
         self.logger.write(logging.INFO, 'Initializing...')
@@ -95,12 +109,13 @@ class Recorder():
 
 
             # resize, store into buffer
-            frame_process = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+            frame_process = cv2.resize(frame, (0, 0), fx=0.5, fy=0.5)
             h_process, w_process, _ = frame_process.shape
             h_full, w_full, _ = frame.shape
             frame_pos = 0 if frame_pos + 1 >= self.buffer_length else frame_pos + 1
             self.video_buffer[frame_pos] = frame
             
+
             # hack for making sure service doesn't crash if there's motion right when it starts up
             if startup:
                 self.video_buffer = [frame for _ in range(self.buffer_length)]
@@ -129,11 +144,14 @@ class Recorder():
                     ).start()
 
             cnt = np.count_nonzero(fgmask)
+
             # threshold...filters out images with too much noise
             if cnt * 25 > h_process * w_process:
                 # see if person (class 0) is in predictions
                 if in_record == 0:
                     self.logger.write(logging.INFO, 'motion detected')
+
+                    # process image and run through model
                     tf_frame = transform_images(tf.expand_dims(cv2.cvtColor(frame_process, cv2.COLOR_BGR2RGB), 0))
                     boxes, scores, classes, nums = self.yolo_model.predict(tf_frame)
 
@@ -147,19 +165,22 @@ class Recorder():
                     detected_boxes = boxes[0][:nums[0]][filter_indices]
                     detected_scores = scores[0][:nums[0]][filter_indices]
                     classes = classes[0][:nums[0]]
-                    classes = classes.astype('int')
+                    classes = classes.astype('int')  # convert classes to ints
 
                     if num_out > 0:
+                        # start to save frames to video
                         self.logger.write(logging.INFO, 'Detected objects of interest: {}'.format(class_names[detected_classes]))
                         in_record = 1
                         start_pos = frame_pos - 30
                         ts = datetime.datetime.now().timestamp()
 
+                        # get timestamp and create file names
                         readable = datetime.datetime.fromtimestamp(ts).isoformat()
                         self.logger.write(logging.INFO, 'writing to new file: {}'.format(readable))
                         video_name = 'videos_{}_{}.mp4'.format(self.camera_id, readable)  # create new filename
                         frame_name = 'images_{}_{}.png'.format(self.camera_id, readable)
 
+                        # insert alert into database
                         now = datetime.datetime.now().isoformat()
                         insert_video_query = "INSERT INTO videos (start_time, video_name, first_frame) values ('{}', '{}', '{}')".format(now, video_name, frame_name)
                         self.cnx = mysql.connector.connect(user=os.getenv('MYSQL_USER'), password=os.getenv('MYSQL_ROOT_PASSWORD'), host='mysql', database='surveillance')
@@ -168,18 +189,21 @@ class Recorder():
                         self.cnx.commit()
                         video_id = self.cursor.lastrowid
 
+                        # add detected objects to database
                         for _class in detected_classes:
                             class_name = class_names[int(_class)]
                             insert_object_query = "INSERT INTO detections (video_id, type) values ('{}', '{}')".format(video_id, class_name)
                             self.cursor.execute(insert_object_query)
                             self.cnx.commit()
 
+                        # draw bounding boxes and create video writer
                         out = cv2.VideoWriter(os.path.join('..', 'files', video_name), self.fourcc, 15.0, (w_full, h_full))
                         frame = draw_outputs(frame,
                             (detected_boxes, detected_scores, detected_classes, num_out),
                             class_names
                         )
 
+                        # save captured frame
                         cv2.imwrite(os.path.join('..', 'files', frame_name), frame)
                     else:
                         self.logger.write(logging.INFO, 'found objects not interested in: {}'.format(class_names[classes]))
