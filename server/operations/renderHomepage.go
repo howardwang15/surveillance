324be47753ffa6f733b402425b5af6c4b6dbeb37
howardwang15 <joy_luck_2000@hotmail.com>
2021-01-23 20:25:57 -0800

    added pagination for navigating different pages of images

--- server/operations/renderHomepage.go
+++ server/operations/renderHomepage.go
@@ -22,6 +22,12 @@ type VideoResponse struct {
 	ID        uint64
 }
 
+type Response struct {
+	Videos []VideoResponse
+	NumPages int
+	Page int
+}
+
 func RenderHomePage(w http.ResponseWriter, r *http.Request) {
 	query := r.URL.Query()
 	page := query["page"]
@@ -37,6 +43,13 @@ func RenderHomePage(w http.ResponseWriter, r *http.Request) {
 
 	var videos []models.TempVideo
 	NUM_RESULTS := 20
+	var count int64
+	db.DB.Model(&models.TempVideo{}).Count(&count)
+	numPages := int(count) / NUM_RESULTS
+
+	if pageFetch > numPages {
+		pageFetch = numPages
+	}
 	db.DB.Order("Id desc").Limit(NUM_RESULTS).Offset((pageFetch - 1) * NUM_RESULTS).Find(&videos)
 
 	videoResponses := make([]VideoResponse, len(videos))
@@ -48,10 +61,21 @@ func RenderHomePage(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 
-	template, _ := template.New("photo_gallery.html").Funcs(template.FuncMap{
+	template, err := template.New("photo_gallery.html").Funcs(template.FuncMap{
 		"htmlSafe": func(html string) template.HTML {
 			return template.HTML(html)
 		},
+		"add": func(a int, b int) int {
+			return a + b
+		},
+		"sub": func(a int, b int) int {
+			return a - b
+		},
 	}).ParseFiles("templates/photo_gallery.html")
-	template.Execute(w, videoResponses)
+
+	if err != nil {
+		panic(err)
+	}
+	response := Response{videoResponses, numPages, pageFetch}
+	template.Execute(w, response)
 }
