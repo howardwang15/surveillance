cb49b21a088bb1c35adbfb54710a49e599980511
howardwang15 <joy_luck_2000@hotmail.com>
2021-01-23 17:53:06 -0800

    added .env loading

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.15
 require (
 	github.com/go-sql-driver/mysql v1.5.0
 	github.com/gorilla/mux v1.8.0
+	github.com/joho/godotenv v1.3.0
 	gorm.io/driver/mysql v1.0.3
 	gorm.io/gorm v1.20.11
 )
--- server/app.go
+++ server/app.go
@@ -1,15 +1,20 @@
 package main
 
 import (
-	"net/http"
-
-	"github.com/howardwang15/surveillance/server/operations"
-
 	"github.com/gorilla/mux"
+	"github.com/howardwang15/surveillance/server/db"
+	"github.com/howardwang15/surveillance/server/operations"
+	"github.com/joho/godotenv"
+	"net/http"
+	"os"
 )
 
 func main() {
+	godotenv.Load("../.env")
 	r := mux.NewRouter()
 	r.HandleFunc("/", operations.RenderHomePage)
+	r.PathPrefix("/assets/").Handler(http.StripPrefix("/assets", http.FileServer(http.Dir("../files"))))
+	dsn := os.Getenv("MYSQL_USER") + ":" + os.Getenv("MYSQL_ROOT_PASSWORD") + "@tcp(127.0.0.1:3306)/" + os.Getenv("MYSQL_DB") + "?parseTime=true"
+	db.Init(dsn)
 	http.ListenAndServe(":8080", r)
 }
